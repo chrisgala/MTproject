3fde16a667992bf269c87eadb5073205cee8166c
Chris <chrisgala13@gmail.com>
2022-05-24 22:02:57 +0300

    Add request logging

---
+++ app/Http/Middleware/LogRequests.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Support\Facades\Log;
+class LogRequests
+{
+    public function handle($request, Closure $next)
+    {
+        $request->start = microtime(true);
+        return $next($request);
+    }
+    public function terminate($request, $response): void
+    {
+        $request->end = microtime(true);
+        $this->log($request,$response);
+    }
+    protected function log($request,$response): void
+    {
+        $duration = $request->end - $request->start;
+        $url = $request->fullUrl();
+        $method = $request->getMethod();
+        $ip = $request->getClientIp();
+        $log = "$ip: $method@$url - {$duration}ms \n".
+            "Request : {$request} \n";
+        Log::info($log);
+    }
+}
